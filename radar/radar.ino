b4e89dd7a6bc3f72af30338c3000ff1fb370e9f4
Paulo <pauloafpjunior@hotmail.com>
2019-08-23 17:02:04 -0300

    radar

--- radar/radar.ino
+++ radar/radar.ino
@@ -1,28 +1,35 @@
 #include "NewPing.h"
 
-#define TRIGGER_PIN 10
-#define ECHO_PIN 11
-#define BUZZER_PIN 12
-#define MAX_DISTANCE 3000
+#define TRIGGER_PIN 50
+#define ECHO_PIN 51
+#define BUZZER_PIN 8
+
+#define MAX_DISTANCE 300
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
 void setup() {
   Serial.begin(9600);
   pinMode(BUZZER_PIN, OUTPUT);
-  digitalWrite(BUZZER_PIN, LOW);
+  analogWrite(BUZZER_PIN, LOW);
 }
 
 void loop() {
-  int d1 = sonar.ping_cm();
+  int dist1 = sonar.ping_cm();
   delay(1000);
-  int d2 = sonar.ping_cm();
+  int dist2 = sonar.ping_cm();
+  
+  int dist_dif = dist2 - dist1;
+  if (dist_dif < 0) dist_dif = dist_dif * (-1);
+  
+  Serial.print("Dist Dif: ");
+  Serial.print(dist_dif);
+  Serial.println("cm");      
 
-  Serial.println(d2 - d1);
-
-  if (d2 > 0 && (d2 - d1) > 5) {
-    digitalWrite(BUZZER_PIN, HIGH);    
-    delay(5000);    
-    digitalWrite(BUZZER_PIN, LOW);    
+  if (dist_dif >= 45) {
+    analogWrite(BUZZER_PIN, HIGH);
+  } else {
+    analogWrite(BUZZER_PIN, LOW);        
   }
 }
+  
